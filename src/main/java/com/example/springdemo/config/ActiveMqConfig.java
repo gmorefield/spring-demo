469b713f2eda8ac6db2a742791f08b0548c976b6
Greg Morefield <gmorefie@yahoo.com>
2022-11-06 21:32:02 -0500

    Added ActiveMq and IbmMq support

---
+++ src/main/java/com/example/springdemo/config/ActiveMqConfig.java
@@ -0,0 +1,30 @@
+package com.example.springdemo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+public class ActiveMqConfig {
+    public static final Logger log = LoggerFactory.getLogger(ActiveMqConfig.class);
+
+    @Configuration
+    @ConditionalOnExpression("T(org.springframework.util.ObjectUtils).isEmpty('${spring.activemq.broker-url:}')")
+    @ImportAutoConfiguration(exclude = { ActiveMQAutoConfiguration.class })
+    public static class Disabled {
+        public Disabled() {
+            log.info("ActiveMq Disabled");
+        }
+    }
+
+    @Configuration
+    @ConditionalOnExpression("T(org.springframework.util.StringUtils).hasText('${spring.activemq.broker-url:}')")
+    public static class Enabled {
+        public Enabled() {
+            log.info("ActiveMq Enabled");
+        }
+    }
+
+}
---
+++ src/main/java/com/example/springdemo/config/IbmMqConfig.java
@@ -0,0 +1,31 @@
+package com.example.springdemo.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Configuration;
+
+import com.ibm.mq.spring.boot.MQAutoConfiguration;
+
+public class IbmMqConfig {
+    public static final Logger log = LoggerFactory.getLogger(IbmMqConfig.class);
+
+    @Configuration
+    @ConditionalOnExpression("T(org.springframework.util.ObjectUtils).isEmpty('${ibm.mq.queueManager:}')")
+    @ImportAutoConfiguration(exclude = { MQAutoConfiguration.class })
+    public static class Disabled {
+        public Disabled() {
+            log.info("IbmMqConfig Disabled");
+        }
+    }
+
+    @Configuration
+    @ConditionalOnExpression("T(org.springframework.util.StringUtils).hasText('${ibm.mq.queueManager:}')")
+    public static class Enabled {
+        public Enabled() {
+            log.info("IbmMqConfig Enabled");
+        }
+    }
+
+}
