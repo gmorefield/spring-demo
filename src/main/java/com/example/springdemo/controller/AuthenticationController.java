719f45726de7b2623a6721029d320d47b1ea0a1f
Greg Morefield <gmorefie@yahoo.com>
2023-12-25 07:39:20 -0500

    Added readRecord to ws

--- src/main/java/com/example/springdemo/controller/AuthenticationController.java
+++ src/main/java/com/example/springdemo/controller/AuthenticationController.java
@@ -1,12 +1,12 @@
 package com.example.springdemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthenticationController {
     
-    @RequestMapping("/authorize")
+    @GetMapping("/authorize")
     public String authorize() {
         return "secure";
     }
