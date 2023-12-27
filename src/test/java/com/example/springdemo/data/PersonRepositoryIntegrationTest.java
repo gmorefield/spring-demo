a5104c9be0c8f4d26cdb3ce3c1bc3ece3806c1cf
Greg Morefield <gmorefie@yahoo.com>
2023-12-27 07:41:54 -0500

    Added use of JdbcTestUtils

--- src/test/java/com/example/springdemo/data/PersonRepositoryIntegrationTest.java
+++ src/test/java/com/example/springdemo/data/PersonRepositoryIntegrationTest.java
@@ -1,25 +1,31 @@
 package com.example.springdemo.data;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
 import com.example.springdemo.model.Person;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTable;
+import static org.springframework.test.jdbc.JdbcTestUtils.countRowsInTableWhere;
 
 @JdbcTest
 @ActiveProfiles("test")
 @Sql({ "/test-person-repo-data.sql" })
 public class PersonRepositoryIntegrationTest {
     private PersonRepository personRepository;
+    private JdbcTemplate jdbcTemplate;
 
-    public PersonRepositoryIntegrationTest(@Autowired NamedParameterJdbcTemplate jdbcTemplate) {
-        personRepository = new PersonRepository(jdbcTemplate);
+    public PersonRepositoryIntegrationTest(@Autowired NamedParameterJdbcTemplate namedJdbcTemplate) {
+        this.jdbcTemplate = namedJdbcTemplate.getJdbcTemplate();
+        personRepository = new PersonRepository(namedJdbcTemplate);
     }
 
     @Test
@@ -51,5 +57,9 @@ public class PersonRepositoryIntegrationTest {
         assertEquals(2, actual.getId());
         assertEquals(expected.getFirstName(), actual.getFirstName());
         assertEquals(expected.getLastName(), actual.getLastName());
+
+        assertThat(countRowsInTable(jdbcTemplate, "Person")).isEqualTo(2);
+        assertThat(countRowsInTableWhere(jdbcTemplate, "Person",
+                "id=2 and first_name='Hans' and last_name='Solo'")).isEqualTo(1);
     }
 }
