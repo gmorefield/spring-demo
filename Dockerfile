d61ffca7564b8269064ef90119fec7577ec2a566
Greg Morefield <gmorefie@yahoo.com>
2023-12-27 07:42:43 -0500

    Changed k8s deployment to use mssql

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ RUN addgroup --system spring && adduser --system spring --ingroup spring
 USER spring:spring
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
-ENV JAVA_TOOL_OPTIONS="-XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=80 -XX:+PrintFlagsFinal"
+ENV JAVA_TOOL_OPTIONS="-XX:MinRAMPercentage=50 -XX:MaxRAMPercentage=80"
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/resources/schema-mssql.sql
+++ src/main/resources/schema-mssql.sql
@@ -1,21 +1,19 @@
--- IF OBJECT_ID(N'dbo.PERSON', N'U') IS NULL
+IF OBJECT_ID(N'dbo.PERSON', N'U') IS NULL
 CREATE TABLE PERSON(
     ID int PRIMARY KEY IDENTITY,
     FIRST_NAME VARCHAR(255),
     LAST_NAME VARCHAR(255)
 );
-GO
 
--- IF OBJECT_ID(N'dbo.APP_EVENT', N'U') IS NULL
+IF OBJECT_ID(N'dbo.APP_EVENT', N'U') IS NULL
 CREATE TABLE APP_EVENT(
     ID varchar(36) PRIMARY KEY,
     CREATE_DT datetime,
     EVENT_BODY varchar(max),
     EVENT_BIN varbinary(max)
 );
-GO
 
--- IF OBJECT_ID(N'dbo.DOCUMENT', N'U') IS NULL
+IF OBJECT_ID(N'dbo.DOCUMENT', N'U') IS NULL
 CREATE TABLE DOCUMENT(
     ID varchar(36) PRIMARY KEY,
     CONTENT_TYPE varchar(255),
@@ -25,7 +23,6 @@ CREATE TABLE DOCUMENT(
     CREATE_DT datetime,
     CHECKSUM varchar(40)
 );
-GO
 
 -- alter table APP_EVENT
 --   add EVENT_BIN varbinary(max);
