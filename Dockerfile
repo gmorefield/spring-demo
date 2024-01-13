c2e800af6ca386615da3e38008fe0d8c4e85116e
Greg Morefield <gmorefie@yahoo.com>
2024-01-13 15:05:31 -0500

    Added MS fonts to Docker image for PlantUML generation

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 # FROM adoptopenjdk/openjdk11
-FROM bellsoft/liberica-openjdk-alpine-musl:17.0.7-7
+FROM bellsoft/liberica-openjdk-alpine-musl:17
 
-RUN apk add --no-cache msttcorefonts-installer fontconfig
-RUN update-ms-fonts
+# RUN apk add --no-cache msttcorefonts-installer fontconfig
+# RUN update-ms-fonts
+
+RUN apk --no-cache add msttcorefonts-installer fontconfig && \
+    update-ms-fonts && \
+    fc-cache -f
 
 RUN addgroup --system spring && adduser --system spring --ingroup spring
 USER spring:spring
--- src/main/java/com/example/springdemo/controller/PlantUmlController.java
+++ src/main/java/com/example/springdemo/controller/PlantUmlController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ContentDisposition;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,23 +18,39 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 @RestController
 @RequestMapping("uml")
 public class PlantUmlController {
-    @PostMapping(path = "generate/{format}")
-    public ResponseEntity<StreamingResponseBody> generateDiagram(@PathVariable("format") @Valid FileFormat fileFormat) {
+    @PostMapping(path = {"generate/{format}", "generate"})
+    public ResponseEntity<StreamingResponseBody> generateDiagram(@PathVariable(value = "format", required = false) @Valid FileFormat fileFormat) {
         String script = "@startuml;component one;@enduml";
         System.out.println("Generate UML for: " + script);
 
+        final FileFormat formatToUse = (fileFormat == null) ? FileFormat.PNG : fileFormat;
         StreamingResponseBody body = outputStream -> {
             SourceStringReader reader = new SourceStringReader(script.replace(';', '\n'));
-            reader.outputImage(outputStream, new FileFormatOption(fileFormat, false));
+            reader.outputImage(outputStream, new FileFormatOption(formatToUse, false));
         };
 
         return ResponseEntity.ok()
-                .contentType(MediaType.valueOf(fileFormat.getMimeType()))
+                .contentType(MediaType.valueOf(formatToUse.getMimeType()))
                 // .contentLength(resource.contentLength())
                 .header(HttpHeaders.CONTENT_DISPOSITION,
                         ContentDisposition.attachment()
-                                .filename("sample" + fileFormat.getFileSuffix())
+                                .filename("sample" + formatToUse.getFileSuffix())
                                 .build().toString())
+                .body(body);
+    }
+
+    @GetMapping(path = {"fonts/{format}","fonts"})
+    public ResponseEntity<StreamingResponseBody> listFonts(@PathVariable(value = "format", required = false) @Valid FileFormat fileFormat) {
+        String script = "@startuml;listfonts;@enduml";
+
+        final FileFormat formatToUse = (fileFormat == null) ? FileFormat.PNG : fileFormat;
+        StreamingResponseBody body = outputStream -> {
+            SourceStringReader reader = new SourceStringReader(script.replace(';', '\n'));
+            reader.outputImage(outputStream, new FileFormatOption(formatToUse, false));
+        };
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(formatToUse.getMimeType()))
                 .body(body);
     }
 }
