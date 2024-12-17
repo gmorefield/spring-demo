432fc847e636d290f6d97b95922fb7f9ea8379b2
Greg Morefield <gmorefie@yahoo.com>
2024-12-17 09:13:11 -0500

    Added Plant UML samples for class diagram and dot installation

--- src/main/java/com/example/springdemo/controller/PlantUmlController.java
+++ src/main/java/com/example/springdemo/controller/PlantUmlController.java
@@ -39,6 +39,36 @@ public class PlantUmlController {
                 .body(body);
     }
 
+    @PostMapping(path = {"class/{format}", "class"})
+    public ResponseEntity<StreamingResponseBody> generateClassDiagram(@PathVariable(value = "format", required = false) @Valid FileFormat fileFormat) {
+        String script = """
+            @startuml
+
+            class "Class1" as c1 {}
+            class "Class2" as c2 {}
+
+            c1 *-- c2
+
+            @enduml
+            """;
+        System.out.println("Generate UML for: " + script);
+
+        final FileFormat formatToUse = (fileFormat == null) ? FileFormat.PNG : fileFormat;
+        StreamingResponseBody body = outputStream -> {
+            SourceStringReader reader = new SourceStringReader(script.replace(';', '\n'));
+            reader.outputImage(outputStream, new FileFormatOption(formatToUse, false));
+        };
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.valueOf(formatToUse.getMimeType()))
+                // .contentLength(resource.contentLength())
+                .header(HttpHeaders.CONTENT_DISPOSITION,
+                        ContentDisposition.attachment()
+                                .filename("sample" + formatToUse.getFileSuffix())
+                                .build().toString())
+                .body(body);
+    }
+
     @GetMapping(path = {"fonts/{format}","fonts"})
     public ResponseEntity<StreamingResponseBody> listFonts(@PathVariable(value = "format", required = false) @Valid FileFormat fileFormat) {
         String script = "@startuml;listfonts;@enduml";
@@ -51,6 +81,21 @@ public class PlantUmlController {
 
         return ResponseEntity.ok()
                 .contentType(MediaType.valueOf(formatToUse.getMimeType()))
+                .body(body);
+    }
+
+    @GetMapping(path = {"test"})
+    public ResponseEntity<StreamingResponseBody> testDot() {
+        String script = "@startuml\ntestdot\n@enduml";
+
+        final FileFormat formatToUse = FileFormat.PNG;
+        StreamingResponseBody body = outputStream -> {
+            SourceStringReader reader = new SourceStringReader(script.replace(';', '\n'));
+            reader.outputImage(outputStream, new FileFormatOption(formatToUse, false));
+        };
+
+        return ResponseEntity.ok()
+                    .contentType(MediaType.valueOf(formatToUse.getMimeType()))
                 .body(body);
     }
 }
