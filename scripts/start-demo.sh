9e9cb850f73833d7f283397435652b6b9fd63977
Greg Morefield <gmorefie@yahoo.com>
2024-12-17 07:53:33 -0500

    Added wiremock examples

--- scripts/start-demo.sh
+++ scripts/start-demo.sh
@@ -6,6 +6,8 @@ elif [ "$group" == "app" ]; then
   kubectl apply -f kconfig/app/ && kubectl -n spring-demo get pods --watch
 elif [ "$group" == "db" ]; then
   kubectl apply -f kconfig/infra/35-sql-edge-kube.yaml -f kconfig/infra/38-sql-edge-init-job.yaml && kubectl -n spring-demo get pods --watch
+elif [ "$group" == "wiremock" ]; then
+  kubectl apply -f kconfig/wiremock/ && kubectl -n wiremock-demo get pods --watch
 else
   kubectl apply -f kconfig/infra/ && sleep 30 && kubectl apply -f kconfig/app/ && kubectl -n spring-demo get pods --watch
 fi
--- scripts/stop-demo.sh
+++ scripts/stop-demo.sh
@@ -6,6 +6,8 @@ elif [ "$group" == "app" ]; then
   kubectl delete -f kconfig/app/
 elif [ "$group" == "db" ]; then
   kubectl delete -f kconfig/infra/35-sql-edge-kube.yaml -f kconfig/infra/38-sql-edge-init-job.yaml
+elif [ "$group" == "wiremock" ]; then
+  kubectl delete -f kconfig/wiremock/
 else
   kubectl delete -f kconfig/app/ && kubectl delete -f kconfig/infra/
 fi
